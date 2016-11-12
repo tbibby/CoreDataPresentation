f0046f9596d9fc797be1a43b60d5501007b21ef0
Thomas Bibby <thomas@bibby.ie>
2016-11-12 11:01:37 +0000

    Initial Commit

---
+++ CoreDataPresentation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataPresentation
+//
+//  Created by Thomas Bibby on 12/11/2016.
+//  Copyright © 2016 Till Now Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreDataPresentation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataPresentation
+//
+//  Created by Thomas Bibby on 12/11/2016.
+//  Copyright © 2016 Till Now Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
